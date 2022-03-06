9d2215bc553a5419e8e860739f0374e6fdfd7768
Can1Cyp2 <99sebastianl@gmail.com>
2022-03-06 17:26:40 -0500

    Plus and minus buttons work, point goal default is 2048 but can be changed

--- src/main/java/com/example/a2048/MainActivity.java
+++ src/main/java/com/example/a2048/MainActivity.java
@@ -15,45 +15,71 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        run();
     }
 
     // TODO: RUNS THE GAME
-    public void run(){
+    public void run() {
         // Makes the grid to start the game
         Gameplay.makeGrid(Gameplay.GRIDSIZE);
 
         boolean loss = false; // if the player lost
-        while (!loss){
+        while (!loss) {
             Gameplay.fillRandomSpot();
 
             // If there are no available spots left and no moves can be made player loses
-            if (Gameplay.endGame()){
+            if (Gameplay.endGame()) {
                 loss = true;
             }
 
         }
-
-        /* TODO Display numbers to the grid by changing the text boxes in the grid in game_screen.xml
-        public void display_grid(){
-
-        } */
-
-
     }
+
+    /* TODO Display numbers to the grid by changing the text boxes in the grid in game_screen.xml
+    public void display_grid(){
+
+     } */
+
+
+
+
+    /* TODO Starts the game: Initializes the grid by calling run, then switches to game screen
+    public void startGame() {
+        // Starts the game and switches the screen when "PLAY" is clicked
+        run();
+        setContentView(R.layout.game_screen);
+    } */
 
     public void pointsUp(View v){
 
-        EditText pointView = (EditText) findViewById(R.id.input_points);
+        TextView pointView = (TextView) findViewById(R.id.input_points);
         String pV = pointView.getText().toString();
 
         int points = Integer.parseInt(pV);
         points *= 2;
+        String goal = (String.valueOf(points));
 
-        ((TextView) findViewById(R.id.input_points)).setText(points);
+        // Max amount of points the user can use
+        if (points <= 16384) {
+            ((TextView) findViewById(R.id.input_points)).setText(goal);
+        }
 
     }
 
+    public void pointsDown(View v){
+
+        TextView pointView = (TextView) findViewById(R.id.input_points);
+        String pV = pointView.getText().toString();
+
+        int points = Integer.parseInt(pV);
+        points /= 2;
+        String goal = (String.valueOf(points));
+
+        // Max amount of points the user can use
+        if (points >= 64) {
+            ((TextView) findViewById(R.id.input_points)).setText(goal);
+        }
+
+    }
 
 }
 
